f1a2426923b6b684f4956fc0acd83b11b6e4bb86
Ionut Cicio <ionutcicio.dev@gmail.com>
2025-11-21 15:08:34 +0100

    fix: .env file names

--- experiments.sh
+++ experiments.sh
@@ -11,7 +11,7 @@ declare -a acq_optimizers=("local_random" "random_scipy" "scipy_global" "cma_es"
 for surrogate_type in "${surrogate_types[@]}"; do
     for acq_type in "${acq_types[@]}"; do
         for acq_optimizer in "${acq_optimizers[@]}"; do
-            filename="experiments/$surrogate_type_$acq_type_$acq_optimizer-2.env"
+            filename="experiments/$surrogate_type-$acq_type-$acq_optimizer-2.env"
             cat experiments/plain.env > $filename
             echo "SURROGATE_TYPE=\"$surrogate_type\"" >> $filename
             echo "ACQ_TYPE=\"$acq_type\"" >> $filename
@@ -21,5 +21,6 @@ for surrogate_type in "${surrogate_types[@]}"; do
 done
 
 for file in experiments/*-2.env; do
-    uv run src/orchestrator.py -e "$file" > $file.log 2>&1
+    echo $file
+    uv run src/orchestrator.py -e $file > $file.log 2>&1
 done
